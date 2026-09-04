2902862337291d027d5624ccea102c9783f35e41
AdrianMendess <adrianlimacristian3@gmail.com>
2026-09-04 16:47:47 -0300

    validação de nome e função de sanitização de input

--- validacao.php
+++ validacao.php
@@ -1,11 +1,43 @@
 <?php
+// Validação de nome
+function sanitizaInput($valor)
+{ // Função para sanitizar input de todas as validações
+    $valor = strip_tags($valor); // remove tags html
+    $valor = trim($valor); //remove espaços no inicio e no final
+    return $valor;
+}
+
+
+$nome = $_POST['nome'];
+
+function validaNome($nome)
+{
+    $nomeLimpo = sanitizaInput($nome);
+    $nome = preg_replace('/[^a-zA-ZÀ-ÿ\s]/', '', $nomeLimpo); //se nao for letras e espaços, remove
+    if (strlen($nome) < 3) {
+        return false;
+    } else {
+        return true;
+    }
+}
+if (validaNome($nome)) {
+    echo ("Nome válido");
+    echo '<br>';
+} else {
+    echo ("Nome inválido");
+    echo '<br>';
+}
+
+
+
 // Validação de CPF
 $cpf = $_POST['cpf'];
 
 function validaCPF($cpf)
 {
+    $cpfLimpo = sanitizaInput($cpf);
     //mostra somente numeros
-    $cpf = preg_replace('/[^0-9]/is', '', $cpf);
+    $cpf = preg_replace('/[^0-9]/is', '', $cpfLimpo);
 
     // Verifica se tem 11 dígitos
     if (strlen($cpf) != 11) {
@@ -45,10 +77,10 @@ if (validaCPF($cpf)) {
 // Validação de Email
 
 $email = $_POST['email'];
-
 function validaEmail($email)
 {
-    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $emailLimpo = sanitizaInput($email);
+    if (filter_var($emailLimpo, FILTER_VALIDATE_EMAIL)) {
         return true;
     } else {
         return false;
@@ -56,10 +88,10 @@ function validaEmail($email)
 }
 
 if (validaEmail($email)) {
-    echo("Email valido");
+    echo ("Email valido");
     echo '<br>';
 } else {
-    echo("Email inválido");
+    echo ("Email inválido");
     echo '<br>';
 }
 
@@ -70,7 +102,8 @@ $telefone = $_POST['telefone'];
 
 function validaTelefone($telefone)
 {
-    $telefone = preg_replace('/[^0-9]/is', '', $telefone);
+    $telefoneLimpo = sanitizaInput($telefone);
+    $telefone = preg_replace('/[^0-9]/is', '', $telefoneLimpo);
 
     if (strlen($telefone) != 11) {
         return false;
@@ -79,10 +112,10 @@ function validaTelefone($telefone)
 }
 
 if (validaTelefone($telefone)) {
-    echo('Telefone válido');
+    echo ('Telefone válido');
     echo '<br>';
 } else {
-    echo("Telefone inválido");
+    echo ("Telefone inválido");
     echo '<br>';;
 }
 
@@ -93,17 +126,20 @@ if (validaTelefone($telefone)) {
 $cidade = $_POST['municipio'];
 $municipios = ['saoLuis', 'raposa', 'lumiar', 'ribamar'];
 
-function validaCidade($cidade, $municipios){
-    if(in_array($cidade, $municipios)){
+function validaCidade($cidade, $municipios)
+{
+    $cidadeLimpa = sanitizaInput($cidade);
+    echo $cidadeLimpa;
+    if (in_array($cidadeLimpa, $municipios)) {
         return true;
     }
     return false;
 }
 
-if(validaCidade($cidade, $municipios)){
-echo("Cidade válida");
-echo '<br>';
+if (validaCidade($cidade, $municipios)) {
+    echo ("Cidade válida");
+    echo '<br>';
 } else {
-    echo("Cidade inválida");
-    echo '<br>';;
+    echo ("Cidade inválida");
+    echo '<br>';
 }
