b2fe44513e0a3514c395d3ab1e9e58fa297af585
AdrianMedess <adrianlimacristian3@gmail.com>
2026-09-15 17:44:51 -0300

    Adição de graficos a dashboard

--- status.js
+++ status.js
@@ -2,6 +2,35 @@ fetch('status_dados.php')
     .then(resposta => resposta.json())
     .then(dados => {
         console.log(dados);
-    }); // testando buscar os dados do php, e retornar os objetos 'porMunicipio" e  "porDia"
 
-    
+        //buusva resultado dos municipios
+        const nomesMunicipios = dados.porMunicipio.map(item => item.municipio);
+        const totaisMunicipios = dados.porMunicipio.map(item => item.total);
+
+        //busca o resultado dos dias
+        const dias = dados.porDia.map(item => item.dia);
+        const totaisDias = dados.porDia.map(item => item.total)
+
+        new Chart(document.getElementById('graficoPorMunicipio'), {
+            type: 'bar',
+            data: {
+                labels: nomesMunicipios,
+                datasets: [{
+                    label: 'Inscrições por Município',
+                    data: totaisMunicipios
+                }]
+            }
+        });
+
+        new Chart(document.getElementById('graficoPorDia'), {
+    type: 'line', 
+    data: {
+        labels: dias,
+        datasets: [{
+            label: 'Inscrições por Dia',
+            data: totaisDias
+        }]
+    }
+});
+
+    });
